b88ca014521170e84f2dfae8c1963bd6d78288c8
Isaac Akakpo <isaacakakpo4@gmail.com>
2023-04-12 11:44:25 +0000

    Initial commit

---
+++ Example/SISwitch/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SISwitch
+//
+//  Created by x-oauth-basic on 04/12/2023.
+//  Copyright (c) 2023 x-oauth-basic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
