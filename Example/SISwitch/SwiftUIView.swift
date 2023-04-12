4a78263527954ce5706f3535c57275b528ef80d1
Isaac Akakpo <isaacakakpo4@gmail.com>
2023-04-12 12:22:32 +0000

    0.1.6

---
+++ Example/SISwitch/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  SISwitch_Example
+//
+//  Created by Isaac Akakpo on 12/04/2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
